80a8d578a94edeed3d9176e6df1edb636b76d4f6
Ruan Montelo <ruanmontelo@gmail.com>
2020-07-22 23:54:35 -0300

    Refactoring a bit more of the classes, separating more methods into seperate classes and renaming the MainTempDirectoryHandler to FilesHandler, since the class is responsible for deleting all files outside sub directories

--- TempFilesKiller/FilesHandler.cs
+++ TempFilesKiller/FilesHandler.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 namespace TempFilesKiller
 {
-    internal static class MainTempDirectoryHandler
+    internal static class FilesHandler
     {
         internal static void DeleteAllFilesInMainTempDirectory(DirectoryInfo mainTempDirectory, string[] arrayOfFilenamesOutsideSubDirectories)
         {
@@ -28,6 +28,24 @@ namespace TempFilesKiller
                 Utils.TreatConditionalMessage("There are no files in the temp folder to delete!");
 
             Console.WriteLine();
+        }
+        internal static void TryToDeleteFilesInDirectory(DirectoryInfo currentDirectory)
+        {
+            FileInfo[] filesInCurrentDirectory = currentDirectory.GetFiles();
+            if (filesInCurrentDirectory.Length > 0)
+            {
+                for (int j = 0; j < filesInCurrentDirectory.Length; j++)
+                {
+                    try
+                    {
+                        filesInCurrentDirectory[j].Delete();
+                    }
+                    catch (Exception ex)
+                    {
+                        Utils.TreatExceptionMessage($"Couldn't delete the file because of the following error: {ex.Message}");
+                    }
+                }
+            }
         }
     }
 }
